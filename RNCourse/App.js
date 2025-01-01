a1e921886598370c18208b8231584ba7e9b699dc
AmjadRasheed7 <1211307882@student.mmu.edu.my>
2025-01-01 10:46:18 +0800

    initial commit

---
+++ RNCourse/App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.textSize}>🍉🍉🍉</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textSize: {
+    fontSize: 90,
+  }
+});
